e4e7f55d9258562e439828c9c9c804da03a9439a
Xardimods <xardimodschannel@gmail.com>
2025-12-09 22:31:01 -0400

    update: changing requirements and expanding bot's logic

---
+++ source/services/telegram_bot.py
@@ -0,0 +1,36 @@
+import asyncio
+import telegram
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, MessageHandler
+
+BOT_TOKEN = "8324365495:AAEfVn7agrFaAP2BnoEA8PhtRvAwpWD4J_0"
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  if update.effective_chat:
+    chat_id = update.effective_chat.id
+    
+    if update.message:
+      if update.message.text:
+        await context.bot.send_message(chat_id=chat_id, text=update.message.text)
+
+logging.basicConfig(
+  format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+  level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+  if update.effective_chat:
+    chat_id = update.effective_chat.id
+    await context.bot.send_message(chat_id=chat_id, text="Soy AInomaly. ¡Puedo detectar anomaías!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
+
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+
+    application.run_polling()
